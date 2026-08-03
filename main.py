8258e4d68497631a06fc20df7436262bc6833297
AsH131211 <aswanth.ash1312@gmail.com>
2026-08-03 14:05:45 +0530

    Initial chatbot project

--- main.py
+++ main.py
@@ -3,16 +3,41 @@ from config import *
 
 client = Groq(api_key=API_KEY)
 
-response = client.chat.completions.create(
-    model=MODEL,
-    messages=[
+messages=[
         {
-            "role": "user",
-            "content": "Hello!"
+            "role": "system",
+            "content": "You are an AI assistant Jarvis."
         }
-    ],
-    temperature=TEMPERATURE,
-    max_completion_tokens=MAX_TOKENS,
-)
+    ]  
 
-print(response.choices[0].message.content)
+while(True):
+
+    prompt = input("> ")
+
+    if prompt.lower() == "exit":
+        print("shutting down...\n")
+        break
+
+    messages.append({
+        "role": "user",
+        "content": prompt
+    })
+
+
+    response = client.chat.completions.create(
+            model=MODEL,
+            temperature=TEMPERATURE,
+            max_completion_tokens=MAX_TOKENS,
+            messages=messages)
+
+    reply = response.choices[0].message.content
+
+
+    print(reply)
+
+    messages.append({
+        "role": "assistant",
+        "content": reply
+    })
+
+
